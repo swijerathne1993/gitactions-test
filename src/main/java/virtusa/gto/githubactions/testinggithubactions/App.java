201260fd1d0e1f35db043da4a75c657974fdaf4d
swijerathne1993 <102947138+swijerathne1993@users.noreply.github.com>
2022-04-04 18:53:23 +0530

    Create App.java

---
+++ src/main/java/virtusa/gto/githubactions/testinggithubactions/App.java
@@ -0,0 +1,13 @@
+package virtusa.gto.githubactions.testinggithubactions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
