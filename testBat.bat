f81e01a94be2d5663f656384f8c0f1e91bb8ac5f
swijerathne1993 <102947138+swijerathne1993@users.noreply.github.com>
2022-04-05 23:45:14 +0530

    Create testBat.bat

---
+++ testBat.bat
@@ -0,0 +1,10 @@
+@echo off
+REM: print new line
+echo.
+echo -----------------------------------------
+echo *** Test Script for parameter ***
+echo -----------------------------------------
+REM: print new line
+echo.
+echo parameterized value is %1
+REM: print new line
