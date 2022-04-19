02941c383c78bbb1db562bb48b5d1f0c136d85f0
swijerathne1993 <102947138+swijerathne1993@users.noreply.github.com>
2022-04-19 12:25:58 +0530

    Create App.java

---
+++ gitactions-test/src/main/java/v/gto/githubactions/testinggithubactions/App.java
@@ -0,0 +1,13 @@
+package v.gto.githubactions.testinggithubactions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
